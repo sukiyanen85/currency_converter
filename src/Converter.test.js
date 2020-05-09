7c027c8ae5b046bd105df88a9291d46bb616aab6
Carlos Duarte <39272982+sukiyanen85@users.noreply.github.com>
2020-05-09 12:47:36 +0100

    chore: test axios request and modal result

--- src/Converter.test.js
+++ src/Converter.test.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Converter from './Converter';
+import { render, fireEvent } from '@testing-library/react';
+import axiosMock from 'axios';
 
-test('Converter component is rending', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Converter/>, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Test Converter Component', () => {
+  it('Converter component is rending', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Converter/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  /** Very important use async, because this test has an asyncronus action ( axios request ) **/
+
+  it('Test a Conversion', async  () => {
+    const { findByTestId, getByTestId } = render(<Converter />);
+
+    /** Mock Axios request */
+    axiosMock.get.mockResolvedValueOnce({
+      data: {
+        success: true,
+        rates: {
+          'EUR': '1',
+          'GBP': '0.88'
+        } 
+      }
+    });
+
+    /** getByTestId is used when the element is present */
+    fireEvent.click(getByTestId('btn-convert'));
+
+    /** findByTestId is used when an asyncronus action need to happen in order to make the action */
+    /** Returns a promise */
+    const modal = await findByTestId('modal');
+
+    /** Confirm that axiosMock.get was called */
+    expect(axiosMock.get).toHaveBeenCalledTimes(1);
+
+    expect(modal).toHaveTextContent('1 EUR = 0.88 GBP');
+  });
 });
+
+
